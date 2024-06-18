fa754d30f38e0ec71f10f3322cb8f9019f7ec735
Toby <79076160+tobyL05@users.noreply.github.com>
2024-06-18 09:31:50 -0700

    replaced next/image for html img

--- components/ui/SearchResult.tsx
+++ components/ui/SearchResult.tsx
@@ -1,5 +1,4 @@
 import { Album } from "../../shared/types";
-import Image from "next/image";
 
 
 interface props {
@@ -10,7 +9,7 @@ export default function SearchResult({ album }: props) {
     return (
         <>
             <div className="flex flex-row items-center space-x-4 rounded-full">
-                <Image src={album.cover} width={100} height={100} alt="album cover"/>
+                <img src={album.cover} width={100} height={100} alt="album cover"/>
                 <div> 
                     <h1 className="text-xl font-semibold">{ album.name}</h1>
                     <h1 className="text-md">{ album.artists.join(", ") }</h1>
